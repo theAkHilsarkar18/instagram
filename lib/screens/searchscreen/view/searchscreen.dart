ef99700c583e7e1617ed562c86cad7d950a2a2f3
theakhilsarkar18 <akhilsodvadiya7@gmail.com>
2023-03-23 20:46:57 +0530

    23-03-2023

--- lib/screens/searchscreen/view/searchscreen.dart
+++ lib/screens/searchscreen/view/searchscreen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:instagram/screens/searchscreen/provider/searchprovider.dart';
 import 'package:provider/provider.dart';
+import 'package:transparent_image/transparent_image.dart';
+
 
 class Searchscreen extends StatefulWidget {
   const Searchscreen({Key? key}) : super(key: key);
@@ -16,32 +18,37 @@ class _SearchscreenState extends State<Searchscreen> {
     Searchprovider searchproviderFalse = Provider.of(context, listen: false);
     Searchprovider searchproviderTrue = Provider.of(context, listen: true);
     return SafeArea(
-      child: GridView.custom(
-        gridDelegate: SliverWovenGridDelegate.count(
-          crossAxisCount: 3,
-          mainAxisSpacing: 5,
-          crossAxisSpacing: 5,
-          pattern: [
-            WovenGridTile(1,alignment: AlignmentDirectional.topStart),
-            WovenGridTile(1,alignment: AlignmentDirectional.topStart),
-            WovenGridTile(
-              5 / 8,
-              alignment: AlignmentDirectional.centerEnd,
-            ),
-          ],
-        ),
-        childrenDelegate: SliverChildBuilderDelegate(
-              (context, index) => postBox(),
-        ),
+      child: Scaffold(
       ),
     );
   }
 
   Widget postBox() {
     return Container(
-      height: 150,
-      width: 150,
+      height: 100,
+      width: 100,
       color: Colors.yellow,
     );
   }
+  Widget postBox2() {
+    return Container(
+      height: 200,
+      width: 100,
+      color: Colors.yellow,
+    );
+  }
+
+  List<String> imageList = [
+    'https://cdn.pixabay.com/photo/2019/03/15/09/49/girl-4056684_960_720.jpg',
+    'https://cdn.pixabay.com/photo/2020/12/15/16/25/clock-5834193__340.jpg',
+    'https://cdn.pixabay.com/photo/2020/09/18/19/31/laptop-5582775_960_720.jpg',
+    'https://media.istockphoto.com/photos/woman-kayaking-in-fjord-in-norway-picture-id1059380230?b=1&k=6&m=1059380230&s=170667a&w=0&h=kA_A_XrhZJjw2bo5jIJ7089-VktFK0h0I4OWDqaac0c=',
+    'https://cdn.pixabay.com/photo/2019/11/05/00/53/cellular-4602489_960_720.jpg',
+    'https://cdn.pixabay.com/photo/2017/02/12/10/29/christmas-2059698_960_720.jpg',
+    'https://cdn.pixabay.com/photo/2020/01/29/17/09/snowboard-4803050_960_720.jpg',
+    'https://cdn.pixabay.com/photo/2020/02/06/20/01/university-library-4825366_960_720.jpg',
+    'https://cdn.pixabay.com/photo/2020/11/22/17/28/cat-5767334_960_720.jpg',
+    'https://cdn.pixabay.com/photo/2020/12/13/16/22/snow-5828736_960_720.jpg',
+    'https://cdn.pixabay.com/photo/2020/12/09/09/27/women-5816861_960_720.jpg',
+  ];
 }
